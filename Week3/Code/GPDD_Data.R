7f04f86823720f2684046fa8c4bae56ef0d83c67
Ruth <ruthkeane166@gmail.com>
2019-10-24 00:50:51 +0100

    completed PP_Regress

--- Week3/Code/GPDD_Data.R
+++ Week3/Code/GPDD_Data.R
@@ -13,4 +13,8 @@ lat<-gpdd$lat
 long<-gpdd$long
 points(long,lat,col="blue",cex=0.5)
 
+#points limited mostly to north america and europe. 
+#absences likely to show lack of surverying rather than absense of the species.
+#lack of data below the equator
+#additionally, very small data frame
 
--- Week3/Code/PP_Regress.R
+++ Week3/Code/PP_Regress.R
@@ -31,23 +31,64 @@ H<-H+theme_bw()+theme(plot.margin = unit(c(.5,4.5,.5,4.5),"cm"))+theme(legend.po
 H<-H+xlab("Prey Mass in grams")+ylab("Predator mass in grams")
 ###didnt workH<-H+labs(fill=element_text(face = "bold"))
 #loop that subsets by feeding and ..., (temp subsets), then lin regression
-e=c(levels(MyDFA$Type.of.feeding.interaction))
-d=c(levels(MyDFA$Predator.lifestage))
-for (n in e) {
-  for (o in d){
-    T<-subset(MyDFA, MyDFA$Predator.lifestage==o)
-    T<-subset(MyDFA, MyDFA$Type.of.feeding.interaction==n)
-    
-  }
-    
-}
-#
-#lim_mod <- summary(lm(Predator.mass ~ Prey.mass, data=MyDFA))
-levels(MyDFA$Type.of.feeding.interaction)
-
-MyDF$Prey.mass.unit==mg
 pdf("../Results/PP_Regress.pdf", 8, 10)
 print(H)
 graphics.off()
 
+##In addition, the script should calculate the regression results corresponding 
+#to the lines fitted in the figure and save it to a csv delimited table called #
+#(PP_Regress_Results.csv), in the Results directory. (Hint: you will have to initialize
+#a new dataframe in the script to first store the calculations and then write.csv() or
+#write.table() it.)
+#All that you are being asked for here is results of an analysis of Linear 
+#regression on subsets of the data corresponding to available Feeding Type ×
+#Predator life Stage combination — not a multivariate linear model with these
+#two as separate covariates!
+#The regression results should include the following with appropriate headers 
+#(e.g., slope, intercept, etc, in each Feeding type ×
+#life stage category): regression slope, regression intercept, R2, F-statistic 
+#value, and p-value of the overall regression (Hint: Review the Stats week!).
 
+e=c(levels(MyDFA$Type.of.feeding.interaction))
+d=c(levels(MyDFA$Predator.lifestage))
+Matrix<-matrix(NA,nrow=30,ncol=7)
+colnames(Matrix)<-c("Predator_lifestage", "Type_of_feeding_interaction", "Slope_of_regression", "Intercept_of_regression", "R-squared", "F-statistic", "p-value")
+S=0
+for (n in e) {
+  for (o in d){
+    S=S+1
+    T<-subset(MyDFA, MyDFA$Predator.lifestage==o)
+    T<-subset(T, T$Type.of.feeding.interaction==n)
+    if (dim(T)[1]<3) {
+      Matrix[S,3]<-NA 
+      Matrix[S,4]<-NA 
+      Matrix[S,5]<-NA 
+      Matrix[S,6]<-NA 
+      Matrix[S,7]<-NA
+      Matrix[S,1]<-NA
+      Matrix[S,2]<-NA
+      } else {
+    lim_mod <- summary(lm(logPredator.mass ~ logPrey.mass, data=T))
+    Matrix[S,3]<-lim_mod$coefficients[2,1]   
+    Matrix[S,4]<-lim_mod$coefficients[1,1]
+    Matrix[S,5]<-lim_mod$r.squared
+    Matrix[S,6]<-lim_mod$fstatistic[1]
+    Matrix[S,7]<-lim_mod$coefficients[2,4]
+    Matrix[S,1]<-o
+    Matrix[S,2]<-n
+      }   
+     }
+}
+M<-na.omit(Matrix)
+write.csv(M,"../Results/PP_Regress_Results.csv")
+
+#lim_mod <- summary(lm(logPredator.mass ~ logPrey.mass, data=MyDFA))
+#
+#lim_mod <- summary(lm(Predator.mass ~ Prey.mass, data=MyDFA))
+#levels(MyDFA$Type.of.feeding.interaction)
+
+#MyDF$Prey.mass.unit==mg
+
+#R<-subset(MyDFA, MyDFA$Predator.lifestage=="postlarva/juvenile")
+#R<-subset(R, R$Type.of.feeding.interaction=="piscivorous")
+#R
