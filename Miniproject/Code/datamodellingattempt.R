2c26946de3f91eae6b7efb656019d34e1aed19bc
Ruth <ruthkeane166@gmail.com>
2019-11-22 17:58:56 +0000

    annotated

--- Miniproject/Code/datamodellingattempt.R
+++ Miniproject/Code/datamodellingattempt.R
@@ -1,48 +1,76 @@
 require("minpack.lm")
 #require("browser")
 
-
+#load data
 data<-read.csv("../Data/CRatsort.csv")
 
 
+#function to find the best value for 
+Maxlm<-function(subsetT){
+  aobest<-vector()
+  v=0
+  
+  #h0=max(subsetT$N_TraitValue)
+  while (nrow(subsetT)>1){
+    v=v+1#subsetbelowht
+    #subsetT<-subset(subsetT, which(subsetT$N_TraitValue < h0))
+  #  subsetT<-
+ #     subsetT$N_TraitValue[which(max(subsetT$N_TraitValue))]
+    #this is wrong!! abive- maYBE TRY WHICH
+    hm<-which(subsetT$N_TraitValue[] == max(subsetT$N_TraitValue[],na.rm=TRUE))
+    subsetT<-subsetT[-hm,]
+    if (nrow(subsetT)>2){
+  #linear model to find gradient- either for all or some values depending on number of values{
+      modht=lm(N_TraitValue~ResDensity,data=subsetT)
+    #h0=max(subsetT$N_TraitValue)
+      #aobest[v,1]<-h0
+      aobest[v]<-as.numeric(modht$coefficients[2])
+    
+    }
+    hm<-0
+  }
+ # maxH<-which(aobest[]==max(aobest[]),na.)
+  #mh<-aobest[maxH]
+  mh<-max(aobest[],na.rm=TRUE)
+  return(mh)
+}
 #to find initial values
 Start<- function(subsetT) {
   h0=max(subsetT$N_TraitValue)
   #ht to find gradient of points below highest point
-  ht=0.95*h0
-  #subsetbelowht
-  subseth<-subset(subsetT,subsetT$N_TraitValue < ht)
-    #linear model to find gradient- either for all or some values depending on number of values
-    if (nrow(subseth)>2) {
-    modht=lm(N_TraitValue~ResDensity,data=subseth)
-    #linear model for all values to find gradient if very few values
-    } else {
-    modht=lm(N_TraitValue~ResDensity,data=subsetT) }
+  a0=Maxlm(subsetT)
+ # hm<-which(subsetT$N_TraitValue[] == max(subsetT$N_TraitValue[],na.rm=TRUE))
+  #subsetT<-subsetT[-hm,]
+  
+  #subsetT<-subset(subsetT,subsetT$N_TraitVal < mout)
   
   ##obtain gridient from model
-  a0<-as.numeric(modht$coefficients[2])
+  #modht=lm(N_TraitValue~ResDensity,data=subsetT)
+  #a0<-as.numeric(modht$coefficients[2])
   ##make matrix for initial values and aic
   mat<-matrix(nrow=200,ncol=4)
   #mat<-matrix(nrow=100,ncol=3)
   #for each row in matrix, sample around initial values and calculate model and AIC 
+  
+  
   for (k in 1:(nrow(mat))){
     #print(j)
-    vala<-rnorm(1,mean=a0, 1)
-    valh<-rnorm(1,mean=h0, 1)
-    valq<-rnorm(1,mean=1.5, 1)
+    vala<-abs(rnorm(1,mean=a0, 1))
+    valh<-abs(rnorm(1,mean=h0, 1))
+    valq<-abs(rnorm(1,mean=1.5, 1))
     #valq<-0
     mat[k,1]<-vala
     mat[k,2]<-valh
     mat[k,3]<-valq
-    HolFit<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=vala, h=valh, q=valq)),silent=TRUE)
+    HolFit<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=vala, h=valh, q=valq), lower=c(0,0,-10)), silent=TRUE)
     #HolFit<-try(nlsLM(N_TraitValue ~ Holling(ResDensity,a,h), data=subsetT, start=list(a=vala, h=valh)),silent=TRUE)
     if("try-error" %in% class(HolFit)){        
       mat[k,4]<-"error"  
-    #calculate AIC for sucessful
-      } else {
+      #calculate AIC for sucessful
+    } else {
       mat[k,4]<-AIC(HolFit) 
       #mat[j,3]<-AIC(HolFit)  
-      }
+    }
   }
   #min(mat[,3])
   #minAIC<-which.min(mat[,3])
@@ -64,14 +92,14 @@ Start<- function(subsetT) {
 
 
 Holling<- function(xR,a,h){
- return((a*xR)/(1+(h*a*xR)))
+  return((a*xR)/(1+(h*a*xR)))
 }
 #}
 HollingGEN<- function(xR,a,h,q){
-  return(((a*xR)^(q+1))/(1+(h*a*xR)^(q+1)))
+  return(((a*xR)^(q+1))/(1+((h*a*xR)^(q+1))))
 }
 #
-#l#istID<-39910
+#listID<-39910
 listID<-as.numeric(unique(data$ID))
 #listID<-c(39841,39839,39842)
 issuesH<-vector()
@@ -94,15 +122,15 @@ for (i in listID){
   #starting values
   out<-Start(subsetT)
   #model fitting
-  HolFit<-try(nlsLM(N_TraitValue ~ Holling(ResDensity,a,h), data=subsetT, start=list(a=out[1],h=out[2])),silent=TRUE)
-  HolFitGEN<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=out[1],h=out[2],q=out[3])),silent=TRUE)
+  HolFit<-try(nlsLM(N_TraitValue ~ Holling(ResDensity,a,h), data=subsetT, start=list(a=out[1],h=out[2]),lower=c(0,0)),silent=TRUE)
+  HolFitGEN<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=out[1],h=out[2],q=out[3]),lower=c(0,0,-10)),silent=TRUE)
   pol<-try(lm(N_TraitValue ~ poly(ResDensity, degree=2), data=subsetT), silent=TRUE)
   polc<-try(lm(N_TraitValue ~ poly(ResDensity, degree=3), data=subsetT),silent=TRUE)
   Lengths<- seq(min(subsetT$ResDensity), max(subsetT$ResDensity),len=200)  
   pdf(paste0("../Results/Holgraphs/",i,".pdf") )
   plot(subsetT$ResDensity,subsetT$N_TraitValue) 
   if(!("try-error" %in% class(polc))){
-  #   &(AIC(polc)!="-Inf")){
+    #   &(AIC(polc)!="-Inf")){
     Predic2Plotpolc<- predict.lm(polc, data.frame(ResDensity=Lengths) )
     lines(Lengths,Predic2Plotpolc,col="#FF6600",lwd=2.5) 
     if (AIC(polc)!=-Inf){
@@ -115,20 +143,20 @@ for (i in listID){
     issuesPC<-append(issuesPC,i)
     modelqual[j,5]<-"NA"
     modelqualb[j,5]<-"NA"
-
-    }
+    
+  }
   if("try-error" %in% class(pol)){
     issuesP<-append(issuesP,i)
     modelqual[j,4]<-"NA"
     modelqualb[j,4]<-"NA"
     
-      }else{
+  }else{
     Predic2Plotpol<- predict.lm(pol, data.frame(ResDensity=Lengths) )
     lines(Lengths,Predic2Plotpol,col="red",lwd=2.5)
     modelqual[j,4]<-AIC(pol)
     modelqualb[j,4]<-BIC(pol)
     
-      }
+  }
   if("try-error" %in% class(HolFit)){        
     issuesH<-append(issuesH,i)
     modelqual[j,2]<-"NA"
@@ -144,9 +172,9 @@ for (i in listID){
     modelqual[j,2]<-AIC(HolFit)
     modelqualb[j,2]<-BIC(HolFit)
     
-        #lines(Lengths,Predic2Plotpol,col="red",lwd=2.5) 
+    #lines(Lengths,Predic2Plotpol,col="red",lwd=2.5) 
   }
-if("try-error" %in% class(HolFitGEN)){        
+  if("try-error" %in% class(HolFitGEN)){        
     issuesG<-append(issuesG,i)
     #pdf(paste0("../Results/Holgraphs/",i,".pdf") )
     #plot(subsetT$ResDensity,subsetT$N_TraitValue) 
@@ -158,7 +186,7 @@ if("try-error" %in% class(HolFitGEN)){
     #   #first need to generate x so can calc predicted ys
     Predic2PlotHolGEN<- HollingGEN(Lengths, coef(HolFitGEN)["a"], coef(HolFitGEN)["h"] , coef(HolFitGEN)["q"])
     lines(Lengths,Predic2PlotHolGEN,col="green",lwd=2.5)
-# pdf(paste0("../Results/Holgraphs/",i,".pdf") )
+    # pdf(paste0("../Results/Holgraphs/",i,".pdf") )
     #plot(subsetT$ResDensity,subsetT$N_TraitValue) 
     #lines(Lengths,Predic2Plotpol,col="red",lwd=2.5) 
     modelqual[j,3]<-AIC(HolFitGEN)  
@@ -168,31 +196,31 @@ if("try-error" %in% class(HolFitGEN)){
   graphics.off()
   ##try to move into above loop?
   #s=0
- #for (l in listID) {
-    #s=s+1
- Best[j,1]<-i
-    #t<-which(modelqual[,1]==l)
-    #print(t)
-   # Best[s,2]<-
- ###not callign correct valye
- t<-which(modelqual[,1]==i)
- u<-which(modelqualb[,1]==i)
- Best[j,2]<- which(modelqual[t,2:5]==min(as.numeric(modelqual[t,2:5]),na.rm = TRUE))
- Best[j,4]<- which(modelqualb[u,2:5]==min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE))
- 
- #na.rm=TRUE, arr.ind=TRUE)
+  #for (l in listID) {
+  #s=s+1
+  Best[j,1]<-i
+  #t<-which(modelqual[,1]==l)
+  #print(t)
+  # Best[s,2]<-
+  ###not callign correct valye
+  t<-which(modelqual[,1]==i)
+  u<-which(modelqualb[,1]==i)
+  Best[j,2]<- which(modelqual[t,2:5]==min(as.numeric(modelqual[t,2:5]),na.rm = TRUE))
+  Best[j,4]<- which(modelqualb[u,2:5]==min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE))
+  
+  #na.rm=TRUE, arr.ind=TRUE)
   #Best[j,2]<- which(modelqual[j,2:5]==a), na.rm=TRUE, arr.ind=FALSE))
- 
- Best[j,3]<-min(as.numeric(modelqual[t,2:5]),na.rm=TRUE)
- Best[j,5]<-min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE)
- 
-    #this isnt working bc l isn't an index
-#  }
+  
+  Best[j,3]<-min(as.numeric(modelqual[t,2:5]),na.rm=TRUE)
+  Best[j,5]<-min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE)
+  
+  #this isnt working bc l isn't an index
+  #  }
 }
 
-write.csv("../Results/CRatBest.csv")
+write.csv(Best,"../Results/CRatBest.csv")
 
-  #minAIC<-which(mat[,4] == min(mat[,4],na.rm=TRUE,arr.ind=TRUE))
+#minAIC<-which(mat[,4] == min(mat[,4],na.rm=TRUE,arr.ind=TRUE))
 
 #     #AICdiff<-AIC(PowFit)-AIC(QuaFit)
 #     #print(paste("AIC difference for", i, "is", AICdiff))
---
+++ Miniproject/Code/datamodellingtidied.R
@@ -0,0 +1,169 @@
+require("minpack.lm")
+
+#load data
+data<-read.csv("../Data/CRatsort.csv")
+
+
+#function to find the steepest possible curve
+Maxlm<-function(subsetT){
+  #empty vector to collect coefficients for linear model
+  aobest<-vector()
+  v=1
+  #run lm for all data and store results in abobest
+  modht=lm(N_TraitValue~ResDensity,data=subsetT)
+  aobest[v]<-as.numeric(modht$coefficients[2])
+  #find hm(maximum value) and remove this value. if sufficient, run lm and store coefficients
+  while (nrow(subsetT)>1){
+    v=v+1
+    hm<-which(subsetT$N_TraitValue[] == max(subsetT$N_TraitValue[],na.rm=TRUE))
+    subsetT<-subsetT[-hm,]
+    if (nrow(subsetT)>2){
+      modht=lm(N_TraitValue~ResDensity,data=subsetT)
+      aobest[v]<-as.numeric(modht$coefficients[2])
+    }
+    #delete values for hm
+    hm<-0
+  }
+  #find largest coefficient
+  mh<-max(aobest[],na.rm=TRUE)
+  return(mh)
+}
+#function to find initial values for a0,h0 and q
+Start<- function(subsetT) {
+  h0=max(subsetT$N_TraitValue)
+  #fun function to calculate steepest gradient
+  a0=Maxlm(subsetT)
+  ##make matrix for initial values and aic
+  mat<-matrix(nrow=200,ncol=4)
+  #for each row in matrix, sample around initial values and calculate AIC 
+  for (k in 1:(nrow(mat))){
+    vala<-abs(rnorm(1,mean=a0, 1))
+    valh<-abs(rnorm(1,mean=h0, 1))
+    valq<-abs(rnorm(1,mean=1.5, 1))
+    mat[k,1]<-vala
+    mat[k,2]<-valh
+    mat[k,3]<-valq
+    HolFit<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=vala, h=valh, q=valq), lower=c(0,0,-10)), silent=TRUE)
+    #HolFit<-try(nlsLM(N_TraitValue ~ Holling(ResDensity,a,h), data=subsetT, start=list(a=vala, h=valh)),silent=TRUE)
+    if("try-error" %in% class(HolFit)){        
+      mat[k,4]<-"error"  
+      #calculate AIC for sucessful
+    } else {
+      mat[k,4]<-AIC(HolFit) 
+    }
+  }
+  #find initial values with the lowest AIC
+  minAIC<-which(mat[,4] == min(mat[,4],na.rm=TRUE,arr.ind=TRUE))
+  
+  initA<-mat[minAIC,1]
+  initH<-mat[minAIC,2]
+  initQ<-mat[minAIC,3]
+  
+  return(c(initA,initH,initQ)) }
+#holling and generalised holling functions
+Holling<- function(xR,a,h){
+  return((a*xR)/(1+(h*a*xR)))
+}
+HollingGEN<- function(xR,a,h,q){
+  return(((a*xR)^(q+1))/(1+((h*a*xR)^(q+1))))
+}
+#listID<-39910
+listID<-as.numeric(unique(data$ID))
+#listID<-c(39841,39839,39842)
+#make empty vectors for errors- can probably remove this
+issuesH<-vector()
+issuesG<-vector()
+issuesPC<-vector()
+issuesP<-vector()
+#matrix for model aic values
+modelqual<-matrix(nrow=length(listID),ncol=5)
+#matrix for model bic values
+modelqualb<-matrix(nrow=length(listID),ncol=5)
+j=0
+#matrix for id, best model and value of aic or bic (best model depending on whether AIC or BIC is used)
+Best<-matrix(ncol=5,nrow=length(listID))
+#for each of different IDs
+#j used to index each of list id
+for (i in listID){
+  j=j+1
+  modelqual[j,1]<-i
+  modelqualb[j,1]<-i
+  #subset data
+  subsetT<-subset(data, ID==i)
+  colnames(modelqual)<-c("ID","Holling","Holling_General","Pol_Quadratic","Pol_Cubic")
+  colnames(modelqualb)<-c("ID","Holling","Holling_General","Pol_Quadratic","Pol_Cubic")
+  #starting values
+  out<-Start(subsetT)
+  #model fitting
+  HolFit<-try(nlsLM(N_TraitValue ~ Holling(ResDensity,a,h), data=subsetT, start=list(a=out[1],h=out[2]),lower=c(0,0)),silent=TRUE)
+  HolFitGEN<-try(nlsLM(N_TraitValue ~ HollingGEN(ResDensity,a,h,q), data=subsetT, start=list(a=out[1],h=out[2],q=out[3]),lower=c(0,0,-10)),silent=TRUE)
+  pol<-try(lm(N_TraitValue ~ poly(ResDensity, degree=2), data=subsetT), silent=TRUE)
+  polc<-try(lm(N_TraitValue ~ poly(ResDensity, degree=3), data=subsetT),silent=TRUE)
+  #x values for model plotting
+  Lengths<- seq(min(subsetT$ResDensity), max(subsetT$ResDensity),len=200)  
+  #make empty pdf for each ID
+  pdf(paste0("../Results/Holgraphs/",i,".pdf") )
+  plot(subsetT$ResDensity,subsetT$N_TraitValue) 
+  #if error in model-then NA in table, otherwise plot and put AIC and BIC in tables
+  #polc
+  if(!("try-error" %in% class(polc))){
+    Predic2Plotpolc<- predict.lm(polc, data.frame(ResDensity=Lengths) )
+    lines(Lengths,Predic2Plotpolc,col="#FF6600",lwd=2.5) 
+    #only use values that are finite
+    if (AIC(polc)!=-Inf){
+      modelqual[j,5]<-AIC(polc)}
+    if (BIC(polc)!=-Inf){
+      modelqualb[j,5]<-BIC(polc)}
+      }else{
+    issuesPC<-append(issuesPC,i)
+    modelqual[j,5]<-"NA"
+    modelqualb[j,5]<-"NA"
+    #pol
+  }
+  if("try-error" %in% class(pol)){
+    issuesP<-append(issuesP,i)
+    modelqual[j,4]<-"NA"
+    modelqualb[j,4]<-"NA"
+    
+  }else{
+    Predic2Plotpol<- predict.lm(pol, data.frame(ResDensity=Lengths) )
+    lines(Lengths,Predic2Plotpol,col="red",lwd=2.5)
+    modelqual[j,4]<-AIC(pol)
+    modelqualb[j,4]<-BIC(pol)
+    #holling
+  }
+  if("try-error" %in% class(HolFit)){        
+    issuesH<-append(issuesH,i)
+    modelqual[j,2]<-"NA"
+    modelqualb[j,2]<-"NA"
+  } else {
+    Predic2PlotHol<- Holling(Lengths, coef(HolFit)["a"], coef(HolFit)["h"])
+    lines(Lengths,Predic2PlotHol,col="blue",lwd=2.5) 
+    modelqual[j,2]<-AIC(HolFit)
+    modelqualb[j,2]<-BIC(HolFit)
+    ##generalised holling
+      }
+  if("try-error" %in% class(HolFitGEN)){        
+    issuesG<-append(issuesG,i)
+    modelqual[j,3]<-"NA"
+    modelqualb[j,3]<-"NA"
+    
+  } else {
+    Predic2PlotHolGEN<- HollingGEN(Lengths, coef(HolFitGEN)["a"], coef(HolFitGEN)["h"] , coef(HolFitGEN)["q"])
+    lines(Lengths,Predic2PlotHolGEN,col="green",lwd=2.5)
+    modelqual[j,3]<-AIC(HolFitGEN)  
+    modelqualb[j,3]<-BIC(HolFitGEN)  
+  }
+  graphics.off()
+#find best values for modelqual and modelqual b. then put in matrix
+  Best[j,1]<-i
+  t<-which(modelqual[,1]==i)
+  u<-which(modelqualb[,1]==i)
+  Best[j,2]<- which(modelqual[t,2:5]==min(as.numeric(modelqual[t,2:5]),na.rm = TRUE))
+  Best[j,4]<- which(modelqualb[u,2:5]==min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE))
+  
+  Best[j,3]<-min(as.numeric(modelqual[t,2:5]),na.rm=TRUE)
+  Best[j,5]<-min(as.numeric(modelqualb[u,2:5]),na.rm=TRUE)
+}
+
+write.csv(Best,"../Results/CRatBest.csv")
